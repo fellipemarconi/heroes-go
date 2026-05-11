2c6edd1e158e595409b806cd659a11767a4226f6
fellipemarconi <anksrr@proton.me>
2026-05-11 19:29:40 -0300

    refactor: adding send validator validation

--- internal/pkg/apierror/error.go
+++ internal/pkg/apierror/error.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type Response struct {
@@ -14,12 +16,29 @@ func Send(w http.ResponseWriter, err error) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var appErr *AppError
-
 	if errors.As(err, &appErr) {
 		w.WriteHeader(appErr.Status)
 
 		_ = json.NewEncoder(w).Encode(Response{
 			Message: appErr.Message,
+		})
+
+		return
+	}
+
+	var validationErr validator.ValidationErrors
+	if errors.As(err, &validationErr) {
+		fields := make(map[string]string)
+
+		for _, fieldErr := range validationErr {
+			fields[fieldErr.Field()] = fieldErr.Tag()
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		_ = json.NewEncoder(w).Encode(map[string]any{
+			"message": "validation failed",
+			"errors":  fields,
 		})
 
 		return
