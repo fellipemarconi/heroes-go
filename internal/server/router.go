40743b897c942293705502b0460b7519fa5ef02c
fellipemarconi <anksrr@proton.me>
2026-05-22 21:51:47 -0300

    feat: adding docs route

--- internal/server/router.go
+++ internal/server/router.go
@@ -6,22 +6,25 @@ import (
 	"api/internal/module/file"
 	"api/internal/module/hero"
 	"api/internal/module/user"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 func NewRouter(db *pgxpool.Pool, queries *sqlc.Queries) chi.Router {
 	r := chi.NewRouter()
-
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
 	// Public routes
 	r.Group(func(r chi.Router) {
 		user.RoutesAuth(db, queries, r)
+
+		setupDocsRoutes(r)
 	})
 
 	// Private routes
@@ -35,4 +38,15 @@ func NewRouter(db *pgxpool.Pool, queries *sqlc.Queries) chi.Router {
 	})
 
 	return r
+}
+
+func setupDocsRoutes(r chi.Router) {
+	r.Get("/docs/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/x-yaml")
+		http.ServeFile(w, r, "./docs/openapi.yaml")
+	})
+
+	r.Get("/docs/*", httpSwagger.Handler(
+		httpSwagger.URL("/docs/openapi.yaml"),
+	))
 }
